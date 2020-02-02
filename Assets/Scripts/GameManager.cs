400a0eacc15512799f9a8a7c1c082e4d06005092
David Friday <44662509+tom1011@users.noreply.github.com>
2020-02-01 23:40:47 -0600

    end of night savepoint
    was trying to set up gamemanage to import my waypoint to a waypoint list so I can have that waypoint list then added to prefab for the enemy so that they will spawn in with all the waypoints to follow

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+     List<Transform> _wayPoints;
+    public List<Transform> Waypoints { get { return _wayPoints; } set { _wayPoints = value; } }
+ 
+    static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+            }
+            return _instance;
+        }
+        set { _instance = value; }
+    }
+ 
+    void Awake()
+    {
+        Instance = this;      
+     }
+ 
+     void Start()
+     {
+        // Initialise waypoints here
+        // _waypoints =
+
+     }
+}

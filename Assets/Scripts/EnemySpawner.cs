feb3b3a7e9256b80e48310101668cffe5f3fd1ff
David Friday <44662509+tom1011@users.noreply.github.com>
2020-02-01 23:08:50 -0600

    prefab start

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Object;
+    
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 2, 1);
+    }
+    
+    void SpawnObject()
+    {
+        float x = Random.Range(-28, -28.3f);
+        float z = Random.Range(131f, 133f);
+        Instantiate(Object, new Vector3(x, 2, z), Quaternion.identity);
+    }
+}
